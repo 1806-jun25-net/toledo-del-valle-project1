864cf621e31a3815cdfad5f057b36775cd459e58
rolov3 <rolando.toledo827@gmail.com>
2018-07-03 20:18:34 -0400

    Added some classes to the library without implementation

---
+++ Project1/Project1.Library/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.Library
+{
+    public class User
+    {
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        private string Location { get; set; }
+
+        public User(string name, string lastName, string location)
+        {
+            Name = name;
+            LastName = lastName;
+            Location = location;
+        }
+
+        public void PlaceOrder()
+        {
+        }
+
+    }
+}
