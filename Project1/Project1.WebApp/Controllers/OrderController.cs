97038eb005ab8298f6d851b69c1295e076bf1ee2
rolov3 <rolando.toledo827@gmail.com>
2018-07-16 09:19:17 -0400

    added time validation

--- Project1/Project1.WebApp/Controllers/OrderController.cs
+++ Project1/Project1.WebApp/Controllers/OrderController.cs
@@ -273,89 +273,101 @@ namespace Project1.WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult NewOrder(OrderW webOrder, IFormCollection collection)
         {
+            TimeSpan timeSpan;
+
             // get all of the data from TempData and collection
             var location = collection["Location"];
             var orderName = TempData.Get<String>("orderName");
             var orderWeb = TempData.Get<OrderW>(orderName);
 
-            // Place the orderName and Order in the TempData in case the order cannnot be completed
-            TempData.Put("orderName", orderName);
-            TempData.Put(orderName, orderWeb);
+            timeSpan = DateTime.Now.Subtract(Repo.GetLastOrderFromLocation(orderWeb.User.Id, location).OrderTime);
 
-            // Get the location from the db
-            var locationOrder = Repo.GetLocation(location);
-
-            // And make a LocationW object to check if there are enough ingridients
-            LocationW LocationW = new LocationW
-            {
-                Id = locationOrder.Id,
-                LocationName = locationOrder.LocationName,
-                DoughQ = locationOrder.DoughQ,
-                SouceQ = locationOrder.SouceQ,
-                CheeseQ = locationOrder.CheeseQ,
-                PepperoniQ = locationOrder.PepperoniQ
-            };
-
-            // get all the pizzaz in Data.Pizza for convenience 
-            List<Data.Pizza> pizzas = OrderW.Map(orderWeb.Pizzas);
-
-            // Assign values to the location and time of the order
-            orderWeb.LocationName = location;
-            orderWeb.TimeOfOrder = DateTime.Now;
-
-            // Get the id of the location 
-            int locationId = Repo.GetLocationId(location);
-
-            // Make an Data.Orders object to insert into the db
-            Data.Orders orderD = OrderW.Map(orderWeb, locationId);
-
-            if (LocationW.EnoughIngridients(pizzas)) {
-                // Substract ingridients from the location
-                LocationW.SubstractIngridients(pizzas);
-
-                // Update the location in the db
-                Repo.UpdateLocation(LocationW.Map(LocationW));
-
-                // Add order to the db
-                Repo.AddOrder(orderD);
-
-                // save changes in the db
-                Repo.Save();
-
-                // Get the id of the order
-                int orderId = Repo.GetOrderId(orderD);
-
-                // Get the id of all the pizzas in the order (create a new pizza entry in the db if it doesn't exist)
-                List<int> pizzaIds = Repo.AddPizzas(pizzas);
-
-                // Add data to the junction table
-                foreach (var item in pizzaIds)
-                {
-                    Repo.AddPizzaOrders(orderId, item);
-                }
-
-                // Save changes to the db
-                Repo.Save();
-
-                // Get the price of the order
-                decimal price = Library.Location.OrderPrice(pizzas);
-
-                // Place the id and price in the orderWeb obj
-                orderWeb.Id = orderId;
-                orderWeb.Price = price;
-
-                // Insert the data into TempData to get it in the OrderReview
+            if (timeSpan.Hours >= 2) {
+                // Place the orderName and Order in the TempData in case the order cannnot be completed
                 TempData.Put("orderName", orderName);
                 TempData.Put(orderName, orderWeb);
 
-                // Let the user review his Order
-                return RedirectToAction(nameof(OrderReview), new { id = orderWeb.User.Id });
+                // Get the location from the db
+                var locationOrder = Repo.GetLocation(location);
+
+                // And make a LocationW object to check if there are enough ingridients
+                LocationW LocationW = new LocationW
+                {
+                    Id = locationOrder.Id,
+                    LocationName = locationOrder.LocationName,
+                    DoughQ = locationOrder.DoughQ,
+                    SouceQ = locationOrder.SouceQ,
+                    CheeseQ = locationOrder.CheeseQ,
+                    PepperoniQ = locationOrder.PepperoniQ
+                };
+
+                // get all the pizzaz in Data.Pizza for convenience 
+                List<Data.Pizza> pizzas = OrderW.Map(orderWeb.Pizzas);
+
+                // Assign values to the location and time of the order
+                orderWeb.LocationName = location;
+                orderWeb.TimeOfOrder = DateTime.Now;
+
+                // Get the id of the location 
+                int locationId = Repo.GetLocationId(location);
+
+                // Make an Data.Orders object to insert into the db
+                Data.Orders orderD = OrderW.Map(orderWeb, locationId);
+
+                if (LocationW.EnoughIngridients(pizzas)) {
+                    // Substract ingridients from the location
+                    LocationW.SubstractIngridients(pizzas);
+
+                    // Update the location in the db
+                    Repo.UpdateLocation(LocationW.Map(LocationW));
+
+                    // Add order to the db
+                    Repo.AddOrder(orderD);
+
+                    // save changes in the db
+                    Repo.Save();
+
+                    // Get the id of the order
+                    int orderId = Repo.GetOrderId(orderD);
+
+                    // Get the id of all the pizzas in the order (create a new pizza entry in the db if it doesn't exist)
+                    List<int> pizzaIds = Repo.AddPizzas(pizzas);
+
+                    // Add data to the junction table
+                    foreach (var item in pizzaIds)
+                    {
+                        Repo.AddPizzaOrders(orderId, item);
+                    }
+
+                    // Save changes to the db
+                    Repo.Save();
+
+                    // Get the price of the order
+                    decimal price = Library.Location.OrderPrice(pizzas);
+
+                    // Place the id and price in the orderWeb obj
+                    orderWeb.Id = orderId;
+                    orderWeb.Price = price;
+
+                    // Insert the data into TempData to get it in the OrderReview
+                    TempData.Put("orderName", orderName);
+                    TempData.Put(orderName, orderWeb);
+
+                    // Let the user review his Order
+                    return RedirectToAction(nameof(OrderReview), new { id = orderWeb.User.Id });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Sorry but that location does not have enough ingridients to complete your order");
+                    return NewOrder(orderName);
+                    //return RedirectToAction(nameof(NewOrder), new { newOrder = orderName });
+                }
             }
             else
             {
-                ModelState.AddModelError("", "Sorry but that location does not have enough ingridients to complete your order");
+                ModelState.AddModelError("", $"Your last order from this locations was {timeSpan.Hours} : {timeSpan.Minutes} : {timeSpan.Seconds} ago." +
+                    $"Need to wait at least 2 hours to order from the same location");
                 return NewOrder(orderName);
-                //return RedirectToAction(nameof(NewOrder), new { newOrder = orderName });
             }
         }
         
